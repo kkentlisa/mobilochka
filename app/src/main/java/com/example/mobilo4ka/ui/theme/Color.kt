b9989f608629ebea0be7a11db6861c3ec06fa0ef
kkentlisa <e.kkenig@gmail.com>
2026-03-20 22:56:08 +0700

    добавила idea в gitignore добавиа цвета(Colors.kt), размеры и отступы(Dimens.kt), штрифты(Type.kt), тему с цветами и шрифтами(Theme.kt) добавила главный экран MainScreen.kt c шапкой(с логотипом ТГУ), выпадающим меню(переключение языка) добавила ресурсы strings.xml(русский и английский), логотип для шапки, иконку приложения

--- app/src/main/java/com/example/mobilo4ka/ui/theme/Color.kt
+++ app/src/main/java/com/example/mobilo4ka/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.mobilo4ka.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+object Colors {
+    val tsuBlue = Color(0xFF0072bc)
+    val backgroundLight = Color(0xFFF3F3F3)
+    val surfaceWhite = Color(0xFFFFFFFF)
+    val textDark = Color(0xFF1D1D1D)
+}
