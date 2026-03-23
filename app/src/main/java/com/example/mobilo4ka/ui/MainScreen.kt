9480019c4f5f3183943d1843d42bea6ab49cbbb3
kkentlisa <e.kkenig@gmail.com>
2026-03-23 15:36:10 +0700

    убрала лишние импорты

--- app/src/main/java/com/example/mobilo4ka/ui/MainScreen.kt
+++ app/src/main/java/com/example/mobilo4ka/ui/MainScreen.kt
@@ -13,10 +13,8 @@ import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
