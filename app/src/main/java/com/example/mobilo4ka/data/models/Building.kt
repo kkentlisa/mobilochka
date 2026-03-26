780bf2e40253ca2d4275e2d9d3651452799d2f61
Liza <elstr2007@gmail.com>
2026-03-26 11:20:41 +0700

    Создала классы для зданий и зон

---
+++ app/src/main/java/com/example/mobilo4ka/data/models/Building.kt
@@ -0,0 +1,3 @@
+package com.example.mobilo4ka.data.models
+
+data class Building(val id: Int, val pixels: List<Pair<Int, Int>>, val name: String? = null)
---
+++ app/src/main/java/com/example/mobilo4ka/data/models/Zone.kt
@@ -0,0 +1,3 @@
+package com.example.mobilo4ka.data.models
+
+data class Zone(val name: String, val pixels: List<Pair<Int, Int>>)
