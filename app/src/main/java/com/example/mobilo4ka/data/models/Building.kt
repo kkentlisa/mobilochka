2f99dfe1482190d1e0e289d7e33d2804ec6d4d14
Liza <elstr2007@gmail.com>
2026-03-29 12:30:19 +0700

    Добавила координаты, пропущенные и исправила классы, передаваемые в них значения

--- app/src/main/java/com/example/mobilo4ka/data/models/Building.kt
+++ app/src/main/java/com/example/mobilo4ka/data/models/Building.kt
@@ -1,3 +1,3 @@
 package com.example.mobilo4ka.data.models
 
-data class Building(val id: Int, val pixels: List<Pair<Int, Int>>, val name: String? = null, val entrances: List<Pair<Int, Int>>? = null)
+data class Building(val id: Int, val pixels: List<List<Int>>, val name: String? = null, val entrances: List<List<Int>>? = null)
