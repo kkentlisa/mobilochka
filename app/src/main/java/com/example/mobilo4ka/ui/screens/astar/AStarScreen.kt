54367b51dc8517921add164994b0fe86c279e536
Liza <elstr2007@gmail.com>
2026-03-30 22:09:58 +0700

    добавила карту на экран

--- app/src/main/java/com/example/mobilo4ka/ui/screens/astar/AStarScreen.kt
+++ app/src/main/java/com/example/mobilo4ka/ui/screens/astar/AStarScreen.kt
@@ -8,17 +8,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.mobilo4ka.data.models.Building
+import com.example.mobilo4ka.data.models.GridMap
+import com.example.mobilo4ka.ui.map.MapView
 import com.example.mobilo4ka.ui.system.SetStatusBarColor
 
 @Composable
-fun AStarScreen(){
+fun AStarScreen(
+    gridData: GridMap,
+    buildingsData: List<Building>,
+    zonesData: Map<String, List<List<Int>>>
+){
     SetStatusBarColor(true)
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.surface),
         contentAlignment = Alignment.Center
-    ){
-        Text("A*")
+    ) {
+        AndroidView(
+            factory = { context ->
+                MapView(context).apply {
+                    this.gridMap = gridData
+                    this.buildings = buildingsData
+                    this.zones = zonesData
+
+                    setupInitialView()
+                }
+            },
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
