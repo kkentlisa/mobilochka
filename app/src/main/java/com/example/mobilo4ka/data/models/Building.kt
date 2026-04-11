be317eabb261a292d8cbffe9f357e2ebcf9a5743
Liza <elstr2007@gmail.com>
2026-04-12 02:58:49 +0700

    добавила функции для определения принадлежности зданию и отдачи входа для него
    (cherry picked from commit 690ca2f4969aecda7bb062eb03c43c12d5bd49aa)

--- app/src/main/java/com/example/mobilo4ka/data/models/Building.kt
+++ app/src/main/java/com/example/mobilo4ka/data/models/Building.kt
@@ -1,3 +1,12 @@
 package com.example.mobilo4ka.data.models
 
 data class Building(val id: Int, val pixels: List<List<Int>>, val name: String? = null, val entrances: List<List<Int>>? = null)
+{
+    fun containsPoint(x: Int, y: Int): Boolean {
+        return pixels.any { it[0] == x && it[1] == y }
+    }
+
+    fun getFirstEntrance(): Pair<Int, Int>? {
+        return entrances?.firstOrNull()?.let { Pair(it[0], it[1]) }
+    }
+}
