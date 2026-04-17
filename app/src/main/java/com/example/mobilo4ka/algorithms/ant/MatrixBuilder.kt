355f0e3daf19acbf9d3cc0cea16cd2cbf91a00d0
Liza <elstr2007@gmail.com>
2026-04-17 22:05:03 +0700

    добавила параметр для сохранения всех точек пути

--- app/src/main/java/com/example/mobilo4ka/algorithms/ant/MatrixBuilder.kt
+++ app/src/main/java/com/example/mobilo4ka/algorithms/ant/MatrixBuilder.kt
@@ -8,35 +8,36 @@ class MatrixBuilder(
     private val isWalkable: (Int, Int) -> Boolean,
     private val isBuilding: (Int, Int) -> Boolean,
     private val getBuildingEntrance: (Int, Int) -> Pair<Int, Int>?,
-    private val getBuildingPixels: (Int, Int) -> Pair<Int, Int>?
 ) {
-    fun build(landmarks: List<Building>): Pair<Array<DoubleArray>, List<Pair<Int, Int>>> {
+    fun build(
+        userX: Int, userY: Int,
+        landmarks: List<Building>
+    ): Triple<Array<DoubleArray>, List<Pair<Int, Int>>, MutableMap<Pair<Int, Int>, List<Pair<Int, Int>>>> {
         val n = landmarks.size
-        val dist = Array(n) { DoubleArray(n) }
+        val totalPoints = n + 1
+        val points = mutableListOf(userX to userY) + landmarks.mapNotNull { it.getFirstPixels() }
+        val dist = Array(totalPoints) { DoubleArray(totalPoints) }
+        val pathMap = mutableMapOf<Pair<Int, Int>, List<Pair<Int, Int>>>()
 
-        val points = landmarks.mapNotNull { building ->
-            building.getFirstPixels()
-        }
-
-        for (i in 0 until n) {
-            for (j in i + 1 until n) {
+        for (i in 0 until totalPoints) {
+            for (j in i+1 until totalPoints) {
                 val path = aStar.findPath(
-                    startX = points[i].first,
-                    startY = points[i].second,
-                    targetX = points[j].first,
-                    targetY = points[j].second,
-                    isWalkable = isWalkable,
-                    isBuilding = isBuilding,
-                    getBuildingEntrance = getBuildingEntrance,
+                    points[i].first, points[i].second,
+                    points[j].first, points[j].second,
+                    isWalkable,
+                    isBuilding,
+                    getBuildingEntrance
                 )
-
                 val distance = if (path.isNotEmpty()) path.size.toDouble() else Double.POSITIVE_INFINITY
                 dist[i][j] = distance
                 dist[j][i] = distance
+                if (path.isNotEmpty()) {
+                    pathMap[Pair(i, j)] = path
+                    pathMap[Pair(j, i)] = path.reversed()
+                }
             }
             dist[i][i] = 0.0
         }
-
-        return Pair(dist, points)
+        return Triple(dist, points, pathMap)
     }
 }
